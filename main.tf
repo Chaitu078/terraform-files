4fb84cfb61038bb731bc253e101de8d5c6c57070
Chaitu078 <chaitu777888@gmail.com>
2025-05-15 14:38:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "myserver" {
+tags = {
+Name = "TF-Server"
+}
+
+ami = "ami-0953476d60561c955"
+instance_type = "t2.micro"
+key_name = "docker-sg"
+availability_zone = "us-east-1a"
+
+root_block_device {
+volume_size = 10
+}
+
+count = 1
+}
